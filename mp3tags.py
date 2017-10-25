d518afaa3ae14cf970a4a69bff9bf335170cf9a3
Dmitriy Vorotilin <dvor85@gmail.com>
2017-10-25 17:20:55 +0400

    Добавлена многопоточность Добавлено ограничение запросов в единицу времени

--- mp3tags.py
+++ mp3tags.py
@@ -6,7 +6,8 @@ import os
 import utils
 import argparse
 import eyed3
-
+import threading
+import time
 
 fmt = utils.fmt
 
@@ -44,25 +45,33 @@ def create_parser():
     return parser
 
 
-def request(url, method='get', params=None, **kwargs):
+def request(url, method='get', params=None, sema=None, **kwargs):
+    if sema:
+        sema.acquire()
+    try:
+        params_str = "?" + "&".join((fmt("{0}={1}", *i)
+                                     for i in params.iteritems())) if params is not None and method == 'get' else ""
+#         print(fmt("{t} | {u}{p}", u=url, p=params_str, t=time.time()))
+        if not url:
+            return
+        kwargs.setdefault('allow_redirects', True)
+        kwargs.setdefault('headers', {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) \
+                Chrome/45.0.2454.99 Safari/537.36'})
+        kwargs.setdefault('timeout', 10)
 
-    if not url:
-        return
-    kwargs.setdefault('allow_redirects', True)
-    kwargs.setdefault('headers', {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/45.0.2454.99 Safari/537.36'})
-    kwargs.setdefault('timeout', 10)
-
-    r = requests.request(method, url, params=params, **kwargs)
-    r.raise_for_status()
-    return r
+        r = requests.request(method, url, params=params, **kwargs)
+        r.raise_for_status()
+        return r
+    finally:
+        if sema:
+            sema.release()
 
 
-def getBestImageOf(info):
+def getBestImageOf(info, sema=None):
     for im in info['image']:
         if im['size'] == 'extralarge':
             if im['#text'] != '':
-                r = request(im['#text'])
+                r = request(im['#text'], sema=sema)
                 return {'img_data': r.content, 'mime_type': r.headers['Content-Type']}
     return {'img_data': None, 'mime_type': None}
 
@@ -71,28 +80,56 @@ def getGenres(info):
     return [g['name'] for g in info['tag']]
 
 
-def trackSearch(title):
+def artistGetInfo(artist, sema=None):
+    if not artist:
+        raise ValueError('Artist not set')
+    params = PARAMS.copy()
+    params['artist'] = artist
+    params['method'] = 'artist.getinfo'
+
+    r = request(URL, params=params, sema=sema)
+    info = r.json()
+    if info.get('error'):
+        raise ValueError(fmt("{msg}: {desc}", msg=info.get('message'), desc=LFM_ERRORS[info['error']]))
+
+    return dict(artist=info['artist']['name'],
+                image=getBestImageOf(info['artist'], sema=sema),
+                genres=getGenres(info['artist']['tags']))
+
+
+def trackSearch(title, artist=None, sema=None):
+    if not title:
+        raise ValueError('Title not set')
     params = PARAMS.copy()
     params['track'] = title
+    params['artist'] = artist if artist else ''
     params['method'] = 'track.search'
-    params['limit'] = 1
-    r = request(URL, params=params)
+    params['limit'] = 10
+    r = request(URL, params=params, sema=sema)
     ts = r.json()
     if ts.get('error'):
         raise ValueError(fmt("{msg}: {desc}", msg=ts.get('message'), desc=LFM_ERRORS[ts['error']]))
     if utils.str2int(ts['results']["opensearch:totalResults"]) == 0:
         raise ValueError('No tracks found')
 
-    return dict(artist=ts['results']['trackmatches']['track'][0]['artist'],
-                title=ts['results']['trackmatches']['track'][0]['name'])
+    tracks = ts['results']['trackmatches']['track']
+    for track in tracks:
+        if artist and utils.lower(track['artist']) in utils.lower(artist):
+            return dict(artist=track['artist'],
+                        title=track['name'])
+
+    return dict(artist=tracks[0]['artist'],
+                title=tracks[0]['name'])
 
 
-def getInfo(title, artist=None):
+def trackGetInfo(title, artist=None, sema=None):
+    if not title:
+        raise ValueError('Title not set')
     params = PARAMS.copy()
     params['track'] = title
 
     if not artist:
-        ts = trackSearch(title)
+        ts = trackSearch(title, artist, sema=sema)
         artist = ts['artist']
         title = ts.get('title', title)
 
@@ -101,17 +138,88 @@ def getInfo(title, artist=None):
     params['method'] = 'track.getinfo'
     params['autocorrect'] = 1
 
-    r = request(URL, params=params)
+    r = request(URL, params=params, sema=sema)
     info = r.json()
 
     if info.get('error'):
         raise ValueError(fmt("{msg}: {desc}", msg=info.get('message'), desc=LFM_ERRORS[info['error']]))
 
-    return dict(album=info['track']['album']['title'],
-                image=getBestImageOf(info['track']['album']),
-                artist=info['track']['artist']['name'],
-                title=info['track']['name'],
-                genres=getGenres(info['track']['toptags']))
+    artist = info['track']['artist']['name']
+    title = info['track']['name']
+    if not info['track'].get('album'):
+        ainfo = artistGetInfo(artist, sema=sema)
+        album = ainfo['artist']
+        image = ainfo['image']
+        genres = ainfo['genres']
+    else:
+        album = info['track']['album']['title']
+        image = getBestImageOf(info['track']['album'], sema=sema)
+        genres = getGenres(info['track']['toptags'])
+
+    return dict(album=album,
+                image=image,
+                artist=artist,
+                title=title,
+                genres=genres,
+                )
+
+
+class TimeLimitedSemaphore(threading._Semaphore):
+    def __init__(self, value=1, perseconds=1, verbose=None):
+        threading._Semaphore.__init__(self, value=value, verbose=verbose)
+        self._perseconds = perseconds
+        self._time = 0
+
+    def acquire(self, blocking=1):
+        rc = threading._Semaphore.acquire(self, blocking)
+        if self._Semaphore__value == 0:
+            self._time = time.time()
+        dt = time.time() - self._time
+        if dt < self._perseconds:
+            if blocking:
+                time.sleep(self._perseconds - dt)
+
+        return rc
+
+
+class updateThread(threading.Thread):
+    def __init__(self, src_fn, sema):
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.src_fn = src_fn
+        self.sema = sema
+
+    def run(self):
+        try:
+            afile = eyed3.load(utils.uni(self.src_fn))
+            if not afile.tag.title:
+                raise ValueError(fmt("{fn}: Tag title not set", fn=self.src_fn))
+            info = trackGetInfo(afile.tag.title, afile.tag.artist, sema=self.sema)
+            print(fmt("{0:-^100s}", "-"))
+            print(self.src_fn)
+            afile.tag.artist = utils.uni(info['artist'])
+            print(fmt("artist = {artist}", artist=afile.tag.artist))
+
+            afile.tag.title = utils.uni(info['title'])
+            print(fmt("title = {title}", title=afile.tag.title))
+
+            afile.tag.album = utils.uni(info['album'])
+            print(fmt("album = {album}", album=afile.tag.album))
+
+            for g in info['genres']:
+                afile.tag.genre = utils.uni(g)
+                if afile.tag.genre.id is not None:
+                    print(fmt("genre = {genre}", genre=afile.tag.genre.name))
+                    break
+            try:
+                afile.tag.images.set(3, img_data=info['image']['img_data'], mime_type=info['image']['mime_type'], img_url=None)
+            except Exception as e:
+                print("Error add image")
+
+            afile.tag.save(encoding='utf8')
+
+        except Exception as e:
+            print(utils.true_enc(e.message))
 
 
 def main():
@@ -123,37 +231,16 @@ def main():
     src_path = os.path.normpath(utils.true_enc(options.src_path))
     srclist = utils.rListFiles(src_path)
 
+    Tsema = TimeLimitedSemaphore(5, 1)
+    Msema = threading.Semaphore(5)
+
     for f in srclist:
         try:
-            src_fn = utils.true_enc(os.path.normpath(f))
-            print fmt("{0:-^100s}", "-")
-            print src_fn
-            afile = eyed3.load(utils.uni(src_fn))
-            info = getInfo(afile.tag.title, afile.tag.artist)
-
-            afile.tag.artist = utils.uni(info['artist'])
-            print fmt("artist = {artist}", artist=afile.tag.artist)
-
-            afile.tag.title = utils.uni(info['title'])
-            print fmt("title = {title}", title=afile.tag.title)
-
-            afile.tag.album = utils.uni(info['album'])
-            print fmt("album = {album}", album=afile.tag.album)
-
-            for g in info['genres']:
-                afile.tag.genre = utils.uni(g)
-                if afile.tag.genre.id is not None:
-                    print fmt("genre = {genre}", genre=afile.tag.genre.name)
-                    break
-            try:
-                afile.tag.images.set(3, img_data=info['image']['img_data'], mime_type=info['image']['mime_type'], img_url=None)
-            except Exception as e:
-                print fmt("Error add image: {msg}", msg=utils.true_enc(e.message))
-
-            afile.tag.save(encoding='utf8')
-
+            with Msema:
+                src_fn = utils.true_enc(os.path.normpath(f))
+                updateThread(src_fn, Tsema).start()
         except Exception as e:
-            print utils.true_enc(e.message)
+            print(utils.true_enc(e.message))
 
 
 if __name__ == '__main__':
