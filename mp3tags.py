ed8feb21ac58b0c1604bd9eeff37a707ebed5b94
Dmitriy Vorotilin <dvor85@gmail.com>
2017-10-26 12:01:13 +0400

    Оптимизация

--- mp3tags.py
+++ mp3tags.py
@@ -14,23 +14,39 @@ fmt = utils.fmt
 
 API_KEY = 'b25b959554ed76058ac220b7b2e0a026'
 URL = 'http://ws.audioscrobbler.com/2.0/'
+TAG_COMMENT = u"Fetched from last.fm by mp3tags"
 PARAMS = {'api_key': API_KEY, 'format': 'json'}
 
+
 LFM_ERRORS = {
-    2: "Invalid service - This service does not exist",
+    1: "This error does not exist",
+    2: "Invalid service -This service does not exist",
     3: "Invalid Method - No method with that name in this package",
     4: "Authentication Failed - You do not have permissions to access the service",
     5: "Invalid format - This service doesn't exist in that format",
     6: "Invalid parameters - Your request is missing a required parameter",
     7: "Invalid resource specified",
-    8: "Operation failed - Something else went wrong",
+    8: "Operation failed - Most likely the backend service failed. Please try again.",
     9: "Invalid session key - Please re-authenticate",
     10: "Invalid API key - You must be granted a valid key by last.fm",
     11: "Service Offline - This service is temporarily offline. Try again later.",
+    12: "Subscribers Only - This station is only available to paid last.fm subscribers",
     13: "Invalid method signature supplied",
-    16: "There was a temporary error processing your request. Please try again",
-    26: "Suspended API key - Access for your account has been suspended, please contact Last.fm",
-    29: "Rate limit exceeded - Your IP has made too many requests in a short period"
+    14: "Unauthorized Token - This token has not been authorized",
+    15: "This item is not available for streaming.",
+    16: "The service is temporarily unavailable, please try again.",
+    17: "Login: User requires to be logged in",
+    18: "Trial Expired - This user has no free radio plays left. Subscription required.",
+    19: "This error does not exist",
+    20: "Not Enough Content - There is not enough content to play this station",
+    21: "Not Enough Members - This group does not have enough members for radio",
+    22: "Not Enough Fans - This artist does not have enough fans for for radio",
+    23: "Not Enough Neighbours - There are not enough neighbours for radio",
+    24: "No Peak Radio - This user is not allowed to listen to radio during peak usage",
+    25: "Radio Not Found - Radio station not found",
+    26: "API Key Suspended - This application is not allowed to make requests to the web services",
+    27: "Deprecated - This type of request is no longer supported",
+    29: "Rate Limit Exceded - Your IP has made too many requests in a short period, exceeding our API guidelines"
 }
 
 
@@ -38,10 +54,6 @@ def create_parser():
     parser = argparse.ArgumentParser(prog='mp3tags.py', add_help=True)
     parser.add_argument('src_path',
                         help='Source path template')
-    parser.add_argument('--exiftool', '-e',
-                        help='Path to exiftool', default='exiftool')
-    parser.add_argument('--eyed3',
-                        help='Path to eyeD3', default='eyeD3')
     return parser
 
 
@@ -113,10 +125,11 @@ def trackSearch(title, artist=None, sema=None):
         raise ValueError('No tracks found')
 
     tracks = ts['results']['trackmatches']['track']
-    for track in tracks:
-        if artist and utils.lower(track['artist']) in utils.lower(artist):
-            return dict(artist=track['artist'],
-                        title=track['name'])
+    if artist:
+        for track in tracks:
+            if utils.lower(track['artist']) in utils.lower(artist):
+                return dict(artist=track['artist'],
+                            title=track['name'])
 
     return dict(artist=tracks[0]['artist'],
                 title=tracks[0]['name'])
@@ -131,7 +144,7 @@ def trackGetInfo(title, artist=None, sema=None):
     if not artist:
         ts = trackSearch(title, artist, sema=sema)
         artist = ts['artist']
-        title = ts.get('title', title)
+        title = ts['title']
 
     params['artist'] = artist
     params['track'] = title
@@ -149,18 +162,21 @@ def trackGetInfo(title, artist=None, sema=None):
     if not info['track'].get('album'):
         ainfo = artistGetInfo(artist, sema=sema)
         album = ainfo['artist']
-        image = ainfo['image']
         genres = ainfo['genres']
+        image = ainfo['image']
     else:
         album = info['track']['album']['title']
-        image = getBestImageOf(info['track']['album'], sema=sema)
         genres = getGenres(info['track']['toptags'])
+        image = getBestImageOf(info['track']['album'], sema=sema)
+        if not image['img_data']:
+            ainfo = artistGetInfo(artist, sema=sema)
+            image = ainfo['image']
 
-    return dict(album=album,
-                image=image,
-                artist=artist,
+    return dict(artist=artist,
                 title=title,
                 genres=genres,
+                album=album,
+                image=image,
                 )
 
 
@@ -182,39 +198,50 @@ class TimeLimitedSemaphore(threading._Semaphore):
         return rc
 
 
-class updateThread(threading.Thread):
-    def __init__(self, src_fn, sema):
+class setTagsThread(threading.Thread):
+    def __init__(self, src_fn, sema, print_lock):
         threading.Thread.__init__(self)
         self.daemon = False
         self.src_fn = src_fn
         self.sema = sema
+        self.print_lock = print_lock
 
     def run(self):
         try:
             afile = eyed3.load(utils.uni(self.src_fn))
-            if not afile.tag.title:
-                raise ValueError(fmt("{fn}: Tag title not set", fn=self.src_fn))
-            info = trackGetInfo(afile.tag.title, afile.tag.artist, sema=self.sema)
-            print(fmt("{0:-^100s}", "-"))
-            print(self.src_fn)
-            afile.tag.artist = utils.uni(info['artist'])
-            print(fmt("artist = {artist}", artist=afile.tag.artist))
+            comment = afile.tag.comments
+            if comment.get(u'') and comment.get(u'').text == TAG_COMMENT:
+                return
+            title = afile.tag.title
+            if not title:
+                afile.initTag()
+                title = os.path.splitext(os.path.basename(self.src_fn))[0]
 
-            afile.tag.title = utils.uni(info['title'])
-            print(fmt("title = {title}", title=afile.tag.title))
+            info = trackGetInfo(title, afile.tag.artist, sema=self.sema)
+            with self.print_lock:
+                print(fmt("{0:-^100s}", "-"))
+                print(self.src_fn)
+                afile.tag.artist = utils.uni(info['artist'])
+                print(fmt("artist = {artist}", artist=afile.tag.artist))
 
-            afile.tag.album = utils.uni(info['album'])
-            print(fmt("album = {album}", album=afile.tag.album))
+                afile.tag.title = utils.uni(info['title'])
+                print(fmt("title = {title}", title=afile.tag.title))
 
-            for g in info['genres']:
-                afile.tag.genre = utils.uni(g)
-                if afile.tag.genre.id is not None:
-                    print(fmt("genre = {genre}", genre=afile.tag.genre.name))
-                    break
-            try:
-                afile.tag.images.set(3, img_data=info['image']['img_data'], mime_type=info['image']['mime_type'], img_url=None)
-            except Exception as e:
-                print("Error add image")
+                afile.tag.album = utils.uni(info['album'])
+                print(fmt("album = {album}", album=afile.tag.album))
+
+                for g in info['genres']:
+                    afile.tag.genre = utils.uni(g)
+                    if afile.tag.genre.id is not None:
+                        print(fmt("genre = {genre}", genre=afile.tag.genre.name))
+                        break
+                try:
+                    afile.tag.images.set(3, img_data=info['image']['img_data'],
+                                         mime_type=info['image']['mime_type'], img_url=None)
+                except Exception as e:
+                    print("Error add image")
+
+                comment.set(TAG_COMMENT, u'')
 
             afile.tag.save(encoding='utf8')
 
@@ -232,13 +259,14 @@ def main():
     srclist = utils.rListFiles(src_path)
 
     Tsema = TimeLimitedSemaphore(5, 1)
+    print_lock = threading.RLock()
     Msema = threading.Semaphore(5)
 
     for f in srclist:
         try:
             with Msema:
                 src_fn = utils.true_enc(os.path.normpath(f))
-                updateThread(src_fn, Tsema).start()
+                setTagsThread(src_fn, Tsema, print_lock).start()
         except Exception as e:
             print(utils.true_enc(e.message))
 
